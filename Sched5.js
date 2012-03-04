612efa6e891d515ea7b2564859306d8800f9843b
Tzafrir Rehan <tzafrir.r@gmail.com>
2012-03-04 16:48:26 +0200

    Looking better

--- Sched5.js
+++ Sched5.js
@@ -21,54 +21,9 @@ Sched5 = function(dbName, scheduledCallback, missCallback) {
  * @param {function(boolean)} callback Success/failure callback.
  */
 Sched5.prototype.init = function(callback) {
-  this.initDb(callback);
+  this._initDb(callback);
   this.handleMisses();
   this.startPolling();
-}
-
-Sched5.prototype.initDb = function(callback) {
-  // Only Chrome is supported officially. Chrome's indexedDB implementation is a bit different than
-  // other browsers', pull requests to handle multi browser are welcome.
-  var indexedDB = window.indexedDB || window.webkitIndexedDB;
-  if (!indexedDB) {
-    fail('Sched5 requires a browser with IndexedDB support');
-  }
-  if ('webkitIndexedDB' in window) {
-    window.IDBTransaction = window.webkitIDBTransaction;
-    window.IDBKeyRange = window.webkitIDBKeyRange;
-  }
-  var request = indexedDB.open(this._dbName);
-  request.onsuccess = function(event) {
-
-    var db = event.target.result;
-    this._db = db;
-
-    // Generic error handler.
-    db.onerror = function(e) {
-      console.error(event.target.errorCode);
-    };
-
-    var v = "1.0";
-    if (v != db.version) {
-      var setVrequest = db.setVersion(v);
-
-      // onsuccess is the only place we can create Object Stores
-      setVrequest.onfailure = db.onerror;
-      setVrequest.onsuccess = function(e) {
-        var store = db.createObjectStore(this.STORE_NAME,
-          {keyPath: "timeStamp"});
-        callback(true);
-      };
-    } else {
-      callback(true);
-    }
-  }.bind(this);
-  request.onerror = this.onError(callback);
-}
-
-function fail(callback, message) {
-  callback(false);
-  console.error(message);
 }
 
 /**
@@ -87,39 +42,82 @@ Sched5.prototype.schedule = function(item, timeStamp, callback) {
     "timeStamp" : timeStamp
   });
 
-  request.onsuccess = this.onSuccess(callback);
-  request.onerror = this.onError(callback);
+  request.onsuccess = this._onSuccess(callback);
+  request.onerror = this._onError(callback);
 }
 
-Sched5.prototype.getAllItems = function() {
+Sched5.prototype._initDb = function(callback) {
+  // Only Chrome is supported officially. Chrome's indexedDB implementation is a bit different than
+  // other browsers', pull requests to handle multi browser are welcome.
+  var indexedDB = window.indexedDB || window.webkitIndexedDB;
+  if (!indexedDB) {
+    fail('Sched5 requires a browser with IndexedDB support');
+  }
+  if ('webkitIndexedDB' in window) {
+    window.IDBTransaction = window.webkitIDBTransaction;
+    window.IDBKeyRange = window.webkitIDBKeyRange;
+  }
+  var request = indexedDB.open(this._dbName);
+  var self = this;
+  request.onsuccess = function(event) {
+
+    var db = event.target.result;
+    self._db = db;
+
+    // Generic error handler.
+    db.onerror = function(e) {
+      console.error(event.target.errorCode);
+    };
+
+    var v = "1.0";
+    if (v != db.version) {
+      var setVrequest = db.setVersion(v);
+
+      setVrequest.onfailure = this._onError(callback);
+      setVrequest.onsuccess = function(e) {
+        var store = db.createObjectStore(this.STORE_NAME,
+          {keyPath: "timeStamp"});
+        callback(true);
+      };
+    } else {
+      callback(true);
+    }
+  };
+  request.onerror = this._onError(callback);
+}
+
+function fail(callback, message) {
+  callback(false);
+  console.error(message);
+}
+
+Sched5.prototype._processAllItemsBefore = function(timeStamp, callback) {
   var db = this._db;
-  var trans = db.transaction([this.STORE_NAME], IDBTransaction.READ_WRITE);
+  var trans = db.transaction([this.STORE_NAME], IDBTransaction.READ_ONLY);
   var store = trans.objectStore(this.STORE_NAME);
 
-  // Get everything in the store;
-  var keyRange = IDBKeyRange.lowerBound(0);
+  var keyRange = IDBKeyRange.upperBound(timeStamp);
   var cursorRequest = store.openCursor(keyRange);
 
   cursorRequest.onsuccess = function(e) {
     var result = e.target.result;
-    if(!!result == false)
+    if(!result) {
       return;
-
-    console.log(result.value);
+    }
+    callback(item);
     result.continue();
   };
 }
 
-Sched5.prototype.onSuccess = function(callback) {
+Sched5.prototype._onSuccess = function(callback) {
   return function(event) {
     callback(true);
   }
 }
 
-Sched5.prototype.onError =  function(callback) {
+Sched5.prototype._onError =  function(callback) {
   return function(event) {
     fail(callback, 'Failed with errorCode ' + event.target.errorCode);
-    callback(true);
   };
 }
 
